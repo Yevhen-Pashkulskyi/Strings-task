f1a5a48410edc80b2bfbe218c18dfc7d5a5c7eb3
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-23 16:54:55 +0200

    add class main

---
+++ src/errorSearch/Main.java
@@ -0,0 +1,14 @@
+package errorSearch;
+
+import errorSearch.controller.Controller;
+import errorSearch.model.Model;
+import errorSearch.view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] words = {"brange", "plum", "tomato", "onibn", "grape"};
+        Model model = new Model(words);
+        View view = new View();
+        Controller controller = new Controller(model, view);
+    }
+}
