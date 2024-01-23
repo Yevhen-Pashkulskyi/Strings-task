63361e5e8e122d156a6164ab00db3c036e0ef044
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-23 16:56:36 +0200

    change class Main

--- src/errorSearch/Main.java
+++ src/errorSearch/Main.java
@@ -7,8 +7,11 @@ import errorSearch.view.View;
 public class Main {
     public static void main(String[] args) {
         String[] words = {"brange", "plum", "tomato", "onibn", "grape"};
+
         Model model = new Model(words);
         View view = new View();
         Controller controller = new Controller(model, view);
+
+        controller.process();
     }
 }
