38cb971a2adb1872a6e24dd7bf3574c4ea9fa2ad
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-25 22:58:20 +0200

    add method counter

---
+++ src/searchName/model/Model.java
@@ -0,0 +1,26 @@
+package searchName.model;
+
+public class Model {
+    private static String[] names;
+
+    public Model() {
+        this.names = new String[]{"Bob", "Alice", "Tom", "Lucy", "Bob", "Lisa"};
+    }
+
+    //цей метод рахує скільки є совподінь з масивом
+    public int counter(String name) {
+        int count = 0;
+        // цикл for each записує в перменну n данні з масиву names
+        for (String n : names) {
+            //ця умова виключає пусті індекси та які не совпали
+            if (n != null && n.equalsIgnoreCase(name)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static String[] getNames() {
+        return names;
+    }
+}
